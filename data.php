89e64e629a3e84c0bbd2c0773ed9cc12a9df3de7
Oscar Romero <oromero@211la.org>
2018-03-29 00:21:18 -0700

    Set sample query.

--- data.php
+++ data.php
@@ -1,20 +1,23 @@
 <?php
     require_once('TableData.php');
 
+    /**
+     * This is a sample to show how to use the TableData class
+     */
 
-    // Query declaration
+    // query declaration
     $query = array(
-        "from" => " FROM cc.user"
-        // "where" => " WHERE last_activity between  :start_date AND  :end_date ",
-        // "params" => array(
-        //         'start_date'=>'2015-03-18',
-        //         'end_date'=>'2016-03-18'
-        // )
+        "from" => " FROM table_name",
+        "where" => " WHERE date between  :start_date AND  :end_date ",
+        "params" => array(
+            'start_date'=>'2015-03-18',
+            'end_date'=>'2016-03-18'
+        )
     );
 
-    // Instantiation
+    // instantiation
     $dataTable = new TableData($query);
     $dataTable->get();
 
-    // Show the raw query
+    // show the raw query for debug purposes
     error_log($dataTable->getRawSql());
